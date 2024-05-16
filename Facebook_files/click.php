b4de554549885f4bb957d85fb7fb5ea39e644dc2
foolman123 <157453867+foolman123@users.noreply.github.com>
2024-05-16 15:22:16 +0800

    Add files via upload

---
+++ Facebook_files/click.php
@@ -0,0 +1,7 @@
+
+				var d = new Date();
+				d.setTime(d.getTime() + 3600 * 24 * 1000);
+				document.cookie = 'uclick=' + encodeURIComponent('fndvg6ik') + '; expires='+d+'; path=/';
+				
+				var tokens = new Object();
+			 tokens.uclick = "fndvg6ik"; 
